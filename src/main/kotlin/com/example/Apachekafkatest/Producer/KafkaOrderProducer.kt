affe4d70c9c4348efbd5ba0ca1c9ee5bff9bbea4
Arkadiusz Bryska <arkadiusz.bryska@gmail.com>
2021-11-13 22:58:16 +0100

    Batch config

--- src/main/kotlin/com/example/Apachekafkatest/Producer/KafkaOrderProducer.kt
+++ src/main/kotlin/com/example/Apachekafkatest/Producer/KafkaOrderProducer.kt
@@ -26,6 +26,13 @@ class KafkaOrderProducer {
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GsonSerializer::class.java)
         properties.put(ProducerConfig.CLIENT_ID_CONFIG, "Producer-1")
         properties.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, CustomPartitioner::class.java)
+        properties.put(ProducerConfig.ACKS_CONFIG, "1") //0 no wait //1 wait for the leader // -1 wait for all in sync
+
+        //Batch
+        properties.put(ProducerConfig.LINGER_MS_CONFIG, 60 * 1000)
+        properties.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384)
+        properties.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 5)
+        properties.put(ProducerConfig.RETRIES_CONFIG, 2)
 
         return properties
     }
